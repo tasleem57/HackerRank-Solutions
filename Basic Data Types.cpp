c2fdb41df16c1577fa5cc5ee883a685092d49288
TASLEEM BANU S <120388034+tasleem57@users.noreply.github.com>
2023-03-03 09:12:37 +0530

    Create Basic Data Types.cpp

---
+++ Basic Data Types.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int a;
+    long b;
+    char c=' ';
+    //float d;
+    double d,e;
+    cin>>a>>b>>c>>d>>e;
+    printf("%d\n%ld\n%c\n%.2lf\n%.5lf\n",a,b,c,d,e);
+    return 0;
+}
