b176df689289aeda58381cb5342f68ce2ff629cf
TASLEEM BANU S <120388034+tasleem57@users.noreply.github.com>
2023-03-03 09:14:19 +0530

    Create Arithmetic Operators.py

---
+++ Arithmetic Operators.py
@@ -0,0 +1,12 @@
+a = int(input())
+b = int(input())
+while a <= 10**10 and a >= 1:
+    if b <= 10**10 and b >=1:
+        sum_ = a + b
+        difference = a - b
+        product = a * b
+        print(sum_)
+        print(difference)
+        print(product)
+        break
+        
